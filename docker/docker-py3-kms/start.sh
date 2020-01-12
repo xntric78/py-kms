759a943400ae7ef774206d72a43a39901c70706c
Christopher Pieper <cpieper@remedyhealthmedia.com>
2020-01-12 14:26:14 -0500

    Change Docker to use Ubuntu

--- docker/docker-py3-kms/start.sh
+++ docker/docker-py3-kms/start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # EN: Start daemons
-# RU: Запуск демонов
-cd /home/py-kms
+cd /opt/py-kms/py-kms
 if [ "$SQLITE" == false ];
 then
   if [ "$EPID" == "" ];
@@ -28,12 +27,12 @@ else
       /bin/bash -c "/usr/bin/python3 pykms_Server.py ${IP} ${PORT} -l ${LCID} -c ${CLIENT_COUNT} -a ${ACTIVATION_INTERVAL} -r ${RENEWAL_INTERVAL} -s -w ${HWID} -V ${LOGLEVEL} -F ${LOGFILE} &"
       sleep 5
       /usr/bin/python3 pykms_Client.py ${IP} ${PORT} -m Windows10 &
-      /usr/bin/python3 /home/sqlite_web/sqlite_web.py -H ${IP} -x ${PWD}/clients.db --read-only
+      /usr/local/bin/sqlite_web -H ${IP} -x ${PWD}/clients.db --read-only
     else
       /bin/bash -c "/usr/bin/python3 pykms_Server.py ${IP} ${PORT} -l ${LCID} -c ${CLIENT_COUNT} -a ${ACTIVATION_INTERVAL} -r ${RENEWAL_INTERVAL} -s -w ${HWID} -V ${LOGLEVEL} -F ${LOGFILE} -S ${LOGSIZE} &"
       sleep 5
       /usr/bin/python3 pykms_Client.py ${IP} ${PORT} -m Windows10 &
-      /usr/bin/python3 /home/sqlite_web/sqlite_web.py -H ${IP} -x ${PWD}/clients.db --read-only
+      /usr/local/bin/sqlite_web -H ${IP} -x ${PWD}/clients.db --read-only
     fi
   else
     if [ "$LOGSIZE" == "" ];
@@ -41,12 +40,12 @@ else
       /bin/bash -c "/usr/bin/python3 pykms_Server.py ${IP} ${PORT} -e ${EPID} -l ${LCID} -c ${CLIENT_COUNT} -a ${ACTIVATION_INTERVAL} -r ${RENEWAL_INTERVAL} -s -w ${HWID} -V ${LOGLEVEL} -F ${LOGFILE} &"
       sleep5
       /usr/bin/python3 pykms_Client.py ${IP} ${PORT} -m Windows10 &
-      /usr/bin/python3 /home/sqlite_web/sqlite_web.py -H ${IP} -x ${PWD}/clients.db --read-only
+      /usr/local/bin/sqlite_web -H ${IP} -x ${PWD}/clients.db --read-only
     else
       /bin/sh -c "/usr/bin/python3 pykms_Server.py ${IP} ${PORT} -e ${EPID} -l ${LCID} -c ${CLIENT_COUNT} -a ${ACTIVATION_INTERVAL} -r ${RENEWAL_INTERVAL} -s -w ${HWID} -V ${LOGLEVEL} -F ${LOGFILE} -S ${LOGSIZE} &"
       sleep 5
       /usr/bin/python3 pykms_Client.py ${IP} ${PORT} -m Windows10 &
-      /usr/bin/python3 /home/sqlite_web/sqlite_web.py -H ${IP} -x ${PWD}/clients.db --read-only
+      /usr/local/bin/sqlite_web -H ${IP} -x ${PWD}/clients.db --read-only
     fi
   fi
 fi
